85abd36b647ecfe46df388e02e188884fd713338
Stoian <sto14n@yahoo.com>
2020-10-14 00:34:25 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,150 @@
+// const project_folder = "dist";
+const project_folder = require('path').basename(__dirname);
+const source_folder = "#src";
+
+const fs = require('fs');
+
+const path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/"
+  },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/index.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+  clean: "./" + project_folder + "/"
+}
+
+const { src, dest } = require('gulp');
+const gulp = require("gulp");
+const browsersync = require("browser-sync").create();
+const fileinclude = require('gulp-file-include');
+const del = require('del');
+const scss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const group_media = require('gulp-group-css-media-queries');
+const clean_css = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const webphtml = require('gulp-webp-html');
+const webpcss = require('gulp-webpcss');
+const svgSprite = require('gulp-svg-sprite');
+
+function browserSync() {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/"
+    }, 
+    port: 3000,
+    // notify: false
+  });
+}
+
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
+}
+
+function css() {
+  return src(path.src.css)
+    .pipe( scss({ outputStyle: "expanded" }) )
+    .pipe( group_media() )
+    .pipe( 
+      autoprefixer({
+        overrideBrowserslist: ["last 8 versions"],
+        cascade: true 
+      })
+    )
+    .pipe(webpcss({}))
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe( rename({ extname: ".min.css" }) )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
+
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
+
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: true}],
+        interlaced: true,
+        optimizationLevel: 3 // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+gulp.task('svgSprite', function() {
+  return gulp.src([source_folder + '/iconsprite/*.svg'])
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../icons/icons.svg",
+          // example: true
+        }
+      }
+    }))
+    .pipe(dest([path.build.img]))
+})
+
+function cb() {
+
+}
+
+function watchFiles() {
+  gulp.watch([path.watch.html], html)
+  gulp.watch([path.watch.css], css)
+  gulp.watch([path.watch.js], js)
+  gulp.watch([path.watch.img], images)
+}
+
+function clean() {
+  return del(path.clean)
+}
+
+const build = gulp.series(clean, gulp.parallel(js, css, html, images))
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
